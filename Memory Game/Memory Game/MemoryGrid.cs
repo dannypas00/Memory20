c76df291aba07723297a9ee26cd22cc7959ae112
Nick de Boer <denick15@gmail.com>
2018-10-10 14:40:28 +0200

    added memory grid

---
+++ Memory Game/Memory Game/MemoryGrid.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace Memory_Game
+{
+    public class MemoryGrid
+    {
+        private Grid grid;
+
+        public MemoryGrid(Grid grid, int cols, int rows)
+        {
+            this.grid = grid;
+            InitializeGameGrid(cols, rows);
+            AddLabel();
+        }
+
+        private void InitializeGameGrid(int cols, int rows)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition());
+            }
+
+            for (int i = 0; i < cols; i++)
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+        }
+
+        private void AddLabel()
+        {
+            Label title = new Label();
+            title.Content = "Memory";
+            title.FontSize = 20;
+            title.HorizontalAlignment = HorizontalAlignment.Center;
+
+            Grid.SetColumn(title, 0);
+            grid.Children.Add(title);
+        }
+    }
+}
