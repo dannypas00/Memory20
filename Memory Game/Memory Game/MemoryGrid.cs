6167778d9c61ad3e5acd51b3b7e79d8de6d56508
Nick de Boer <denick15@gmail.com>
2018-10-16 15:38:43 +0200

    HET WERKKKKKKKKKKKKKTTTTTTTT HEUUUUUUUUUUY

--- Memory Game/Memory Game/MemoryGrid.cs
+++ Memory Game/Memory Game/MemoryGrid.cs
@@ -9,19 +9,60 @@ using System.Windows;
 using System.Windows.Media.Imaging;
 using System.Windows.Input;
 
+
 namespace Memory_Game
 {
     public class MemoryGrid
     {
+        int ImageNumber;
+        BitmapImage[] ImageKind = new BitmapImage[16];
+        
+
+        //BitmapImage[] RandomImageKind = new BitmapImage[16];
+        List<string> Gridpoints = new List<string>
+        {
+            "00","01","02","03","10","11","12","13","20","21","22","23","30","31","32","33"
+        };
+        //string[] GridPoints = new string[16] {
+        //     "00",
+        //     "01",
+        //     "02",
+        //     "03",
+        //     "10",
+        //     "11",
+        //     "12",
+        //     "13",
+        //     "20",
+        //     "21",
+        //     "23",
+        //     "24",
+        //     "30",
+        //     "31",
+        //     "32",
+        //     "33"
+        //};
         private Grid grid;
 
         public MemoryGrid(Grid grid, int cols, int rows)
         {
+            for (int i = 0; i < 8; i++)
+            {
+                ImageKind[i] = new BitmapImage(new Uri("ab" + (i + 1) + ".png", UriKind.Relative));
+                ImageKind[i + 8] = new BitmapImage(new Uri("ab" + (i + 1) + ".png", UriKind.Relative));
+            }
+            Random rnd = new Random();
+            ImageKind = ImageKind.OrderBy(x => rnd.Next()).ToArray();
+
+
+            //Random rnd = new Random();
+            //BitmapImage[] RandomImageKind = ImageKind.OrderBy(x => rnd.Next()).ToArray();
 
             this.grid = grid;
             InitializeGameGrid(cols, rows);
             CreateImage(cols, rows);
+
         }
+
 
         private void InitializeGameGrid(int cols, int rows)
         {
@@ -80,12 +121,11 @@ namespace Memory_Game
            
 
             var point = Mouse.GetPosition(grid);
-
             int row = 0;
             int column = 0;
             double accumulatedHeight = 0.0;
             double accumulatedWidth = 0.0;
-
+            
             // calc row mouse was over
             foreach (var rowDefinition in grid.RowDefinitions)
             {
@@ -103,14 +143,35 @@ namespace Memory_Game
                     break;
                 column++;
             }
-            Image AngryBird = new Image();
-            AngryBird.Source = new BitmapImage(new Uri("ab6.png", UriKind.Relative));
-            Grid.SetColumn(AngryBird, column);
-            Grid.SetRow(AngryBird, row);
-            grid.Children.Add(AngryBird);
 
+            Image AngryBird = new Image();
+
+                string abc = Convert.ToString(row) + Convert.ToString(column);
+                Console.WriteLine(abc);
+
+                for (int i=0;i < Gridpoints.Count; i++)
+                {
+                if(Gridpoints[i].Contains(abc))
+                {
+                    ImageNumber = i;
+                    Console.WriteLine("i is: " + i);
+                    Console.WriteLine("ImageNumber is: " + ImageNumber);
+                }
+                }
+                
+
+                AngryBird.Source = ImageKind[ImageNumber];
+                Grid.SetColumn(AngryBird, column);
+                Grid.SetRow(AngryBird, row);
+                grid.Children.Add(AngryBird);
+                //ImageNumber++;
+                
+
+                
+            
 
 
         }
+
     }
 }
