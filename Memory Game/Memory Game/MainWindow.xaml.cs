d4b2086d539e440e48943b50a78449ec4ce4f6f1
Jan Minne <janminne@gmail.com>
2018-10-30 16:28:50 +0100

    Werkende Themakeuze inclusief Menu button, StartPage Click me!

--- Memory Game/Memory Game/MainWindow.xaml.cs
+++ Memory Game/Memory Game/MainWindow.xaml.cs
@@ -28,99 +28,105 @@ namespace Memory_Game
         private const int NR_OF_COLS = 4;
         private const int NR_OF_ROWS = 4;
 
+  
         //Clear the screen
         public void Clear()
         {
             Main.Children.Clear();
         }
-
-
-        //Click on the play button
-        private void PlayBtn_Click(object sender, RoutedEventArgs e)
+        
+        private void Start_Game(object sender, RoutedEventArgs e)
         {
-            Clear();
-            OpenThemakeuze();
-        }
-
-        public void OpenThemakeuze()
-        {
-            Grid Themakeuzegrid = new Grid();
-            Themakeuzegrid.Name = "Themakeuze";
-            Main.Children.Add(Themakeuzegrid);
-            Themakeuzegrid.ShowGridLines = true;
-            themakeuze = new Themakeuze(Main, Themakeuzegrid);
-            //InitializeGameGrid(4, 4);
-            Themakeuzegrid.Background = Brushes.Aqua;
-        }
-
-        ////Click on the Angry Birds Button
-        //private void PlayGameButton(object sender, RoutedEventArgs e)
-        //{
-        //    Clear();
-        //    player1 = PlayerName("Player 1");
-        //    player2 = PlayerName("Player 2");
-        //    OpenGame();
-        //}
-
-        //public string PlayerName(string player)
-        //{
-
-        //    return "Test";
-        //}
-
-        //public void OpenGame()
-        //{
-        //    Grid gameGrid = new Grid();
-        //    gameGrid.Name = "GameGrid";
-        //    Main.Children.Add(gameGrid);
-
-        //    Label playerScores = new Label();
-        //    playerScores.Name = "playerScores";
-        //    playerScores.FontFamily = new FontFamily("Comic Sans MS");
-        //    playerScores.FontSize = 30;
-
-        //    string player1 = Convert.ToString(Player1Name.Text);
-        //    string player2 = Convert.ToString(Player2Name.Text);
-        //    grid = new MemoryGrid(gameGrid, NR_OF_COLS, NR_OF_ROWS, playerScores, player1, player2, Main);
-        //    gameGrid.Background = Brushes.Aqua;
-        //}
-
-
-        //Click on the Highscore Button
-        private void HighscoreBtn_Click(object sender, RoutedEventArgs e)
-        {
-            Clear();
-            OpenHighscore();
-        }
-
-        //Open the highscore page
-        public void OpenHighscore()
-        {
-            Label myLabel = new Label();
-            myLabel.Content = "HIGHSCORES";
-            myLabel.HorizontalAlignment = HorizontalAlignment.Center;
-            myLabel.VerticalAlignment = VerticalAlignment.Top;
-            myLabel.FontSize = 75;
-            Main.Children.Add(myLabel);
-
-            Grid Highscoregrid = new Grid();
-            Highscoregrid.Name = "Highscore";
-            Main.Children.Add(Highscoregrid);
-            Highscoregrid.ShowGridLines = true;
-            highscore = new Highscore(Main, Highscoregrid);
-            //InitializeGameGrid(4, 4);
-            //Highscoregrid.Background = Brushes.Aqua;
-        }
-
-        //Click on the MainMenu Button
-        private void MainMenuButton(object sender, RoutedEventArgs e)
-        {
-            Clear();
             OpenMainMenu();
         }
 
+        ////Click on the play button
+        //private void PlayBtn_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Clear();
+        //    OpenThemakeuze();
+        //}
+
+        //public void OpenThemakeuze()
+        //{
+        //    Grid Themakeuzegrid = new Grid();
+        //    Themakeuzegrid.Name = "Themakeuze";
+        //    Main.Children.Add(Themakeuzegrid);
+        //    Themakeuzegrid.ShowGridLines = true;
+        //    themakeuze = new Themakeuze(Main, Themakeuzegrid);
+        //    //InitializeGameGrid(4, 4);
+        //    Themakeuzegrid.Background = Brushes.Aqua;
+        //}
+
+        //////Click on the Angry Birds Button
+        ////private void PlayGameButton(object sender, RoutedEventArgs e)
+        ////{
+        ////    Clear();
+        ////    player1 = PlayerName("Player 1");
+        ////    player2 = PlayerName("Player 2");
+        ////    OpenGame();
+        ////}
+
+        ////public string PlayerName(string player)
+        ////{
+
+        ////    return "Test";
+        ////}
+
+        ////public void OpenGame()
+        ////{
+        ////    Grid gameGrid = new Grid();
+        ////    gameGrid.Name = "GameGrid";
+        ////    Main.Children.Add(gameGrid);
+
+        ////    Label playerScores = new Label();
+        ////    playerScores.Name = "playerScores";
+        ////    playerScores.FontFamily = new FontFamily("Comic Sans MS");
+        ////    playerScores.FontSize = 30;
+
+        ////    string player1 = Convert.ToString(Player1Name.Text);
+        ////    string player2 = Convert.ToString(Player2Name.Text);
+        ////    grid = new MemoryGrid(gameGrid, NR_OF_COLS, NR_OF_ROWS, playerScores, player1, player2, Main);
+        ////    gameGrid.Background = Brushes.Aqua;
+        ////}
+
+
+        ////Click on the Highscore Button
+        //private void HighscoreBtn_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Clear();
+        //    OpenHighscore();
+        //}
+
+        ////Open the highscore page
+        //public void OpenHighscore()
+        //{
+        //    Label myLabel = new Label();
+        //    myLabel.Content = "HIGHSCORES";
+        //    myLabel.HorizontalAlignment = HorizontalAlignment.Center;
+        //    myLabel.VerticalAlignment = VerticalAlignment.Top;
+        //    myLabel.FontSize = 75;
+        //    Main.Children.Add(myLabel);
+
+        //    Grid Highscoregrid = new Grid();
+        //    Highscoregrid.Name = "Highscore";
+        //    Main.Children.Add(Highscoregrid);
+        //    Highscoregrid.ShowGridLines = true;
+        //    highscore = new Highscore(Main, Highscoregrid);
+        //    //InitializeGameGrid(4, 4);
+        //    //Highscoregrid.Background = Brushes.Aqua;
+        //}
+
+        ////Click on the MainMenu Button
+        //private void MainMenuButton(object sender, RoutedEventArgs e)
+        //{
+        //    Clear();
+        //    OpenMainMenu();
+        //}
+
         private void OpenMainMenu()
         {
+            Clear();
             Grid MainMenugrid = new Grid();
             MainMenugrid.Name = "MainMenu";
             Main.Children.Add(MainMenugrid);
@@ -129,5 +135,6 @@ namespace Memory_Game
             //InitializeGameGrid(4, 4);
             MainMenugrid.Background = Brushes.Aqua;
         }
+
     }
 }
--- Memory Game/Memory Game/MemoryGrid.cs
+++ Memory Game/Memory Game/MemoryGrid.cs
@@ -8,12 +8,18 @@ using System.Windows.Media;
 using System.Windows;
 using System.Windows.Media.Imaging;
 using System.Windows.Input;
-
+using System.Windows.Threading;
 
 namespace Memory_Game
 {
     public class MemoryGrid
     {
+        //Timer that ticks every 0.3 seconds
+        DispatcherTimer timer = new DispatcherTimer();
+        //Timer that starts ticking if you don't get a pair and after one second flips the card back over
+        DispatcherTimer timer2 = new DispatcherTimer();
+        bool timed = true;              //Boolean to check if the flip timer has finished
+
         //Statics
         private Grid grid;              //Variable containing the game grid
         int cols;                       //Amount of columns
@@ -22,13 +28,14 @@ namespace Memory_Game
         List<string> Gridpoints = new List<string> { "00", "01", "02", "03", "10", "11", "12", "13", "20", "21", "22", "23", "30", "31", "32", "33" };
         int[] ImageKind = new int[16];  //Array for connecting images to gridpoints
         bool[] Gridmem = new bool[16];  //Array to remember made pairs
+        int player = 1;
 
         //Temporaries
         int ImageNumbermem;             //Temporary variable containing previouos ImageNumber
-        int tempImageKind = 0;          //Temporary variable containing previous ImageKind
+        int tempImageKind;              //Temporary variable containing previous ImageKind
 
         //Game logic
-        int turn;                       //Int to check weither two turns have elapsed
+        int turn = 0;                       //Int to check weither two turns have elapsed
         int ImageNumber;                //Connector for ImageKind to grid
         string thema = "ab";            //Settable variable containing thema             
               
@@ -39,10 +46,29 @@ namespace Memory_Game
         string playerName2;             //Name of player 2
         int playerScore2;               //Score of player 2
         private Label playerScores;     //Variable containting the label to display player scores
+        private StackPanel Main;
+        private Mainmenu mainmenu;
+
+        //Saving and loading
+        //Path for the save file
+        string path = System.IO.Path.GetFullPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\")) + "Save.sav";
+
 
         //MemoryGrid Class
         public MemoryGrid(Grid grid, int cols, int rows, Label playerScores, string playerName1, string playerName2, StackPanel Main, string thema)
         {
+            //Variables from local to private
+            this.playerName1 = playerName1;
+            this.playerName2 = playerName2;
+            this.grid = grid;
+            this.playerScores = playerScores;
+            this.grid = grid;
+            this.cols = cols;
+            this.rows = rows;
+            this.thema = thema;
+            this.Main = Main;
+
+            //loadSave();
             //Creates the score keeper
             Main.Children.Add(playerScores);
             playerScores.Content = playerName1 + ": " + playerScore1 + "        " + playerName2 + ": " + playerScore2;
@@ -59,22 +85,84 @@ namespace Memory_Game
             Random rnd = new Random();
             ImageKind = ImageKind.OrderBy(x => rnd.Next()).ToArray();
 
-            //Variables from local to private
-            this.playerName1 = playerName1;
-            this.playerName2 = playerName2;
-            this.grid = grid;
-            this.playerScores = playerScores;
-            this.grid = grid;
-            this.cols = cols;
-            this.rows = rows;
-            this.thema = thema;
-
             //Insantiate an empty grid
             InitializeGameGrid(cols, rows);
-            
+
+            //Starts the timers
+            DispatcherTimerSample();
+
             //Create images in grid
             CreateImage(cols, rows);
+
+            Button MainMenuButton = new Button();
+            MainMenuButton.Content = "Main Menu";
+            MainMenuButton.HorizontalAlignment = HorizontalAlignment.Center;
+            MainMenuButton.FontSize = 25;
+            MainMenuButton.Click += MainMenuButton_Click;
+            MainMenuButton.Width = 800;
+
+            Main.Children.Add(MainMenuButton);
         }
+
+        //Save Game
+        private void save()
+        {
+            System.IO.File.WriteAllText(path, "");
+            string[] save = new string[22];
+            save[0] = "playerName1" + playerName1;
+            save[1] = "playerName2" + playerName2;
+            save[2] = "playerScore1" + playerScore1;
+            save[3] = "playerScore2" + playerScore2;
+            save[4] = "turn" + turn;
+            int j = 0;
+            foreach (bool i in Gridmem)
+            {
+                save[5 + j] = "Gridmem" + i;
+                j++;
+            }
+            save[21] = "thema" + thema;
+            System.IO.File.WriteAllLines(path, save);
+        }
+
+        /*
+        //Load Game
+        private void loadSave()
+        {
+            string[] Read = System.IO.File.ReadAllLines(path);
+            foreach (string line in Read)
+            {
+                Console.WriteLine(line);
+            }
+            int runs = 0;
+            foreach (string line in Read)
+            {
+                if (line.Contains("playerName1")) { playerName1 = line.Substring("playerName1".Length); }
+                if (line.Contains("playerName2")) { playerName2 = line.Substring("playerName2".Length); }
+                if (line.Contains("playerScore1")) { playerScore1 = Convert.ToInt32(line.Substring("playerScore1".Length)); }
+                if (line.Contains("playerScore2")) { playerScore2 = Convert.ToInt32(line.Substring("playerScore2".Length)); }
+                if (line.Contains("turn")) { turn = Convert.ToInt32(line.Substring("turn".Length)); }
+                if (line.Contains("Gridmem"))
+                {
+                    switch (line.Substring("Gridmem".Length))
+                    {
+                        case "true":
+                            Gridmem[runs] = true;
+                            break;
+                        case "false":
+                            Gridmem[runs] = false;
+                            break;
+                        default:
+                            break;
+                    }
+                    runs++;
+                }
+                if (line.Contains("thema")) { thema = line.Substring("thema".Length); }
+                grid.Children.Clear();
+                CreateImage(cols, rows);
+            }
+        }*/
+
+
 
 
         //Create Empty grid cols x rows
@@ -101,14 +189,14 @@ namespace Memory_Game
                 {
                     //Create and set content for backgroundImage
                     Image backgroundImage = new Image();
-                    backgroundImage.Name = (thema + "bg") + Convert.ToString(row) + Convert.ToString(column);
+                    backgroundImage.Name = thema + "bg" + Convert.ToString(row) + Convert.ToString(column);
                     backgroundImage.Source = new BitmapImage(new Uri(thema + "bg.png", UriKind.Relative));
                     Grid.SetColumn(backgroundImage, column);
                     Grid.SetRow(backgroundImage, row);
                     grid.Children.Add(backgroundImage);
 
                     //Runs trough Gridmem; Keeps pairs turned around
-                    for(int i =0; i < 16; i++)
+                    for (int i = 0; i < 16; i++)
                     {
                         if (Gridmem[i] == true)
                         {
@@ -135,91 +223,154 @@ namespace Memory_Game
         //Happens on backgroundImage click
         private void OnPreviewMouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            var point = Mouse.GetPosition(grid);
-            int row = 0;
-            int column = 0;
-            double accumulatedHeight = 0.0;
-            double accumulatedWidth = 0.0;
-            // calc row mouse was over
-            foreach (var rowDefinition in grid.RowDefinitions)
+            if (timed == true)
             {
-                accumulatedHeight += rowDefinition.ActualHeight;
-                if (accumulatedHeight >= point.Y)
-                    break;
-                row++;
-            }
-            // calc col mouse was over
-            foreach (var columnDefinition in grid.ColumnDefinitions)
-            {
-                accumulatedWidth += columnDefinition.ActualWidth;
-                if (accumulatedWidth >= point.X)
-                    break;
-                column++;
-            }
-            Image AngryBird = new Image();
-            // Make the grid point into a string so you can reconize it in the list (Gridpoints)
-            string imagePos = Convert.ToString(row) + Convert.ToString(column);
-            turn++;
-            // Match string imagePos to List Gridpoints
-            for (int i = 0; i < Gridpoints.Count; i++)
-            {
-                if (Gridpoints[i].Contains(imagePos))
+                save();
+                var point = Mouse.GetPosition(grid);
+                int row = 0;
+                int column = 0;
+                double accumulatedHeight = 0.0;
+                double accumulatedWidth = 0.0;
+                // calc row mouse was over
+                foreach (var rowDefinition in grid.RowDefinitions)
                 {
-                    ImageNumber = i;
+                    accumulatedHeight += rowDefinition.ActualHeight;
+                    if (accumulatedHeight >= point.Y)
+                        break;
+                    row++;
+                }
+                // calc col mouse was over
+                foreach (var columnDefinition in grid.ColumnDefinitions)
+                {
+                    accumulatedWidth += columnDefinition.ActualWidth;
+                    if (accumulatedWidth >= point.X)
+                        break;
+                    column++;
+                }
+
+                Image AngryBird = new Image();
+                // Make the grid point into a string so you can reconize it in the list (Gridpoints)
+                string imagePos = Convert.ToString(row) + Convert.ToString(column);
+
+                // Match string imagePos to List Gridpoints
+                for (int i = 0; i < Gridpoints.Count; i++)
+                {
+                    if (Gridpoints[i].Contains(imagePos))
+                    {
+                        ImageNumber = i;
+                    }
+                }
+                // Write the Randomized image on the spot you click
+                AngryBird.Source = new BitmapImage(new Uri(thema + (ImageKind[ImageNumber] + 1) + ".png", UriKind.Relative));
+                AngryBird.Name = thema + Convert.ToString(ImageKind[ImageNumber]);
+                Grid.SetColumn(AngryBird, column);
+                Grid.SetRow(AngryBird, row);
+                grid.Children.Add(AngryBird);
+
+                Gridmem[ImageNumber] = true;
+
+                turn++;
+                if (turn != 2)
+                {
+                    ImageNumbermem = ImageNumber;
+                    tempImageKind = ImageKind[ImageNumber];
                 }
             }
-            // Write the Randomized image on the spot you click
-            AngryBird.Source = new BitmapImage(new Uri(thema + (ImageKind[ImageNumber] + 1) + ".png", UriKind.Relative));
-            AngryBird.Name = thema + Convert.ToString(ImageKind[ImageNumber]);
-            Grid.SetColumn(AngryBird, column);
-            Grid.SetRow(AngryBird, row);
-            grid.Children.Add(AngryBird);
-            Console.WriteLine(AngryBird.Name);
-            Gridmem[ImageNumber] = true;
-            Console.WriteLine(Convert.ToString(ImageKind[ImageNumber]));
+        }
 
-            //Debug pairs
 
-            //Console.WriteLine(Convert.ToString(Gridmem[0]));
-            //Console.WriteLine(Convert.ToString(Gridmem[1]));
-            //Console.WriteLine(Convert.ToString(Gridmem[2]));
-            //Console.WriteLine(Convert.ToString(Gridmem[3]));
-            //Console.WriteLine(Convert.ToString(Gridmem[4]));
-            //Console.WriteLine(Convert.ToString(Gridmem[5]));
-            //Console.WriteLine(Convert.ToString(Gridmem[6]));
-            //Console.WriteLine(Convert.ToString(Gridmem[7]));
-            //Console.WriteLine(Convert.ToString(Gridmem[8]));
-            //Console.WriteLine(Convert.ToString(Gridmem[9]));
-            //Console.WriteLine(Convert.ToString(Gridmem[10]));
-            //Console.WriteLine(Convert.ToString(Gridmem[11]));
-            //Console.WriteLine(Convert.ToString(Gridmem[12]));
-            //Console.WriteLine(Convert.ToString(Gridmem[13]));
-            //Console.WriteLine(Convert.ToString(Gridmem[14]));
-            //Console.WriteLine(Convert.ToString(Gridmem[15]));
-
+        private void Turn()
+        {
             //Goes 2 turns at a time
             if (turn > 1)
             {
+                player++;
                 //Check if image is the same
-                if (ImageKind[ImageNumber] + 1 == tempImageKind )
+                if (ImageKind[ImageNumber] == tempImageKind || ImageKind[ImageNumber] == tempImageKind)
                 {
                     Console.WriteLine("PAIR!");
+                    //Checks which player's turn it is
+                    if (player % 2 == 0)
+                    {
+                        playerScore2 += 10;
+                    }
+                    else
+                    {
+                        playerScore1 += 10;
+                    }
+                    player -= 1;
                 }
                 else
                 {
-                    //Resets field minus pairs
-                    Gridmem[ImageNumber] = false;
-                    Gridmem[ImageNumbermem] = false;
-                    grid.Children.Clear();
-                    CreateImage(cols, rows);
+                    timed = false;
+                    timer2.Start();
                 }
                 turn = 0;
             }
-            //Sets temps to remember
-            ImageNumbermem = ImageNumber;
-            tempImageKind = ImageKind[ImageNumber] + 1;
-            //Show player scores
+        }
+
+
+        private void Clear()
+        {
+            Gridmem[ImageNumber] = false;
+            Gridmem[ImageNumbermem] = false;
+            grid.Children.Clear();
+            CreateImage(cols, rows);
+        }
+
+
+        public void DispatcherTimerSample()
+        {
+            timer.Interval = TimeSpan.FromMilliseconds(1);
+            timer2.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer2.Tick += timer2_Tick;
+            timer.Start();
+        }
+
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Turn();
             playerScores.Content = playerName1 + ": " + playerScore1 + "        " + playerName2 + ": " + playerScore2;
         }
+
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Clear();
+            timer2.Stop();
+            timed = true;
+        }
+
+
+
+
+        //Clear the screen
+        public void ClearScreen()
+        {
+            Main.Children.Clear();
+        }
+
+
+
+        //Click on the MainMenu Button
+        private void MainMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Children.Clear();
+            OpenMainMenu(Main);
+        }
+
+        private void OpenMainMenu(StackPanel Main)
+        {
+            Grid MainMenugrid = new Grid();
+            MainMenugrid.Name = "MainMenu";
+            Main.Children.Add(MainMenugrid);
+            MainMenugrid.ShowGridLines = true;
+            mainmenu = new Mainmenu(Main, MainMenugrid);
+            //InitializeGameGrid(4, 4);
+            MainMenugrid.Background = Brushes.Aqua;
+        }
+    
     }
 }
